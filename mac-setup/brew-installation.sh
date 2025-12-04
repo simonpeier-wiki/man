d9a65eeb22e2e829f9f239093ad33b5e18f63e3d
Simon Peier <simon.peier@iway.ch>
2025-12-04 13:51:07 +0100

    Update mac setup and git cheatsheet

--- mac-setup/brew-installation.sh
+++ mac-setup/brew-installation.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-brew install --cask iterm2
-brew install --cask sublime-text
-brew install --cask typora
-brew install --cask insomnia
+# Applications
+brew install --cask firefox
 brew install --cask brave-browser
+brew install --cask iterm2
 brew install --cask jetbrains-toolbox
+brew install --cask sublime-text
+brew install --cask obsidian
+brew install --cask notion
+brew install --cask insomnia
 brew install --cask spotify
-brew install --cask dozer
 brew install --cask rectangle
 brew install --cask vlc
-brew install --cask notion
-brew install --cask firefox
 brew install --cask microsoft-word
 brew install --cask microsoft-powerpoint
 brew install --cask microsoft-excel
+
+# Commandline tools
+brew install tldr
